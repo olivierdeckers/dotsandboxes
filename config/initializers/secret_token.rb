6d22fc52e6926a1ce91d251a9c41d8b9fd90be0a
Olivier Deckers <deckers.olivier@gmail.com>
2014-06-15 19:26:53 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DotsnBoxes::Application.config.secret_key_base = '2f2113f57e5a7bebc59eb78d1805fb3204e564dd2a5b7fbe259db71ea26298d71dc5931336f1e6bbf727e73feafc0129887128de691872c8664af789175f57bd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DotsnBoxes::Application.config.session_store :cookie_store, key: '_DotsnBoxes_session'
